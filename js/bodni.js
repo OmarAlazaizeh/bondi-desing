c2111b11fd4f8bd3cb53961f5b5e350ccdea092f
OmarAlazaizeh <omaralazaizeh4@gmail.com>
2024-06-18 20:28:05 +0300

    Comitted the project files

---
+++ js/bodni.js
@@ -0,0 +1,10 @@
+// let navbarToggler = document.querySelector(".navbar-toggler");
+
+// navbarToggler.addEventListener("click", (event) => {
+//   // console.log(event.target);
+//   // event.target.innerHTML = "test";
+//   // console.log(navbarToggler.classList);
+//   if (navbarToggler.classList.contains("collapsed")) {
+//     navbarToggler.innerHTML = '<i class="fa - solid fa - x"></i>';
+//   }
+// });
